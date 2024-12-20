84659b42436ff14a73e65b2b9aad3a53abb4b269
sealday <sealday@gmail.com>
2024-12-21 06:50:56 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from "react";
+import Sheet from "./sheet";
+
+const App = () => {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current) {
+      new Sheet(ref.current);
+    }
+  }, [ref.current]);
+  return <div ref={ref} />;
+};
+
+export default App;
